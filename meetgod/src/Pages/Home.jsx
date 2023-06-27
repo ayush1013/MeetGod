b4dde7ba07379a4d57087a720d6ac316f75ba12e
Ayush Verma <ayush98277@gmail.com>
2023-06-27 20:27:33 +0530

    post section added

--- meetgod/src/Pages/Home.jsx
+++ meetgod/src/Pages/Home.jsx
@@ -1,12 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Navbar from "../Components/Navbar";
 import MobNav from "../Components/MobNav";
 import { Box, Flex, Grid, Image, Input, Text } from "@chakra-ui/react";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
-import {postData} from "../LocalData/Posts"
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import CommentIcon from "@material-ui/icons/Comment";
+import { postData } from "../LocalData/Posts";
 
 const Home = () => {
   const fileRef = useRef("");
+  const [doLike, setDoLike] = useState(false);
+  const [doComment, setDoComment] = useState(false);
+
+  const handleLike = () => {
+    setDoLike(!doLike);
+  };
+
+  const handleComment = () => {
+    setDoComment(!doComment);
+  };
 
   const handleFileClick = () => {
     fileRef.current.click();
@@ -32,7 +44,7 @@ const Home = () => {
             </Text>
           </Box>
         </Box>
-        <Box w="40%" border="1px solid red">
+        <Box w="40%">
           <Box>
             <Flex
               // border="1px solid black"
@@ -52,7 +64,11 @@ const Home = () => {
                 w="50px"
                 borderRadius={"50%"}
               />
-              <Input placeholder="Share your thoughts" bgColor="#F3F2EF" border="none" />
+              <Input
+                placeholder="Share your thoughts"
+                bgColor="#F3F2EF"
+                border="none"
+              />
 
               <AddPhotoAlternateIcon
                 fontSize="large"
@@ -69,19 +85,118 @@ const Home = () => {
               ></Input>
             </Flex>
           </Box>
-          <Grid>
-            {
-              postData.map((elem)=> {
-                return(
-                  <Box key={elem.id}>
-                    <Flex>
-                      <Image src={elem.profilePic}/>
+          <Grid mt="30px" rowGap={"10px"}>
+            {postData.map((elem) => {
+              return (
+                <Box
+                  key={elem.id}
+                  bgColor={"white"}
+                  borderRadius={"20px"}
+                  p="10px"
+                  border={"1px solid #EBEBEB"}
+                  borderBottom={"2px solid #EBEBEB"}
+                >
+                  <Flex alignItems={"center"}>
+                    <Image
+                      w="40px"
+                      borderRadius={"50%"}
+                      src={elem.profilePic}
+                    />
+                    <Box ml="10px" mt="-5px">
+                      <Text fontWeight={"500"}>{elem.fullName}</Text>
+                      <Text color="#666666" mt="-5px" fontSize={"12px"}>
+                        Devotee of Shiva
+                      </Text>
+                    </Box>
+                  </Flex>
 
-                    </Flex>
+                  <Box mt="10px" w="100%">
+                    <Text fontSize={"14px"} fontWeight={"500"}>
+                      {elem.title}
+                    </Text>
+                    <Text fontSize={"13px"}>{elem.content}</Text>
+                    <Image m="auto" src={elem.imageUrl} mt="10px" />
                   </Box>
-                )
-              } )
-            }
+
+                  <Flex
+                    justifyContent={"space-between"}
+                    p="10px"
+                    borderBottom={"1px solid #EBEBEB"}
+                  >
+                    <Flex alignItems={"center"}>
+                      <ThumbUpAltIcon
+                        style={{
+                          color: "#D0E8FF",
+                          backgroundColor: "#4F83B6",
+                          fontSize: "14px",
+                          padding: "2px",
+                          borderRadius: "50%",
+                        }}
+                      />
+                      <Text fontSize={"12px"} color="#666666">
+                        {elem.likes}
+                      </Text>
+                    </Flex>
+                    <Text fontSize={"12px"} color="#666666">
+                      {elem.comments?.length} Comments
+                    </Text>
+                  </Flex>
+
+                  <Flex
+                    w="80%"
+                    m="auto"
+                    justifyContent={"space-between"}
+                    p="5px"
+                  >
+                    <Flex
+                      alignItems={"center"}
+                      cursor={"pointer"}
+                      p="5px 10px"
+                      borderRadius={"5px"}
+                      _hover={{ backgroundColor: "#F3F2EF" }}
+                      onClick={handleLike}
+                    >
+                      <ThumbUpAltIcon
+                        style={{
+                          color: doLike ? "#4F83B6" : "black",
+                          fontSize: "30px",
+                          padding: "2px",
+                          borderRadius: "50%",
+                        }}
+                      />
+                      <Text
+                        fontSize={"14px"}
+                        color={doLike ? "#4F83B6" : "none"}
+                        fontWeight={"500"}
+                      >
+                        Like
+                      </Text>
+                    </Flex>
+
+                    <Flex
+                      alignItems={"center"}
+                      cursor={"pointer"}
+                      p="5px 10px"
+                      borderRadius={"5px"}
+                      _hover={{ backgroundColor: "#F3F2EF" }}
+                      onClick={handleComment}
+                    >
+                      <CommentIcon
+                        style={{
+                          color: doComment ? "#4F83B6" : "black",
+                          fontSize: "30px",
+                          padding: "2px",
+                          borderRadius: "50%",
+                        }}
+                      />
+                      <Text fontSize={"14px"} color={doComment ? "#4F83B6" : "black"} fontWeight={"500"}>
+                        Comment
+                      </Text>
+                    </Flex>
+                  </Flex>
+                </Box>
+              );
+            })}
           </Grid>
         </Box>
         <Box w="25%" border="1px solid red"></Box>
