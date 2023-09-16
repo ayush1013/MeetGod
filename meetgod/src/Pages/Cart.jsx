64e1b1af1acc1672557aa54e060606c0ff63da6a
Ayush Verma <ayush98277@gmail.com>
2023-09-16 20:37:43 +0530

    made cart component responsive

--- meetgod/src/Pages/Cart.jsx
+++ meetgod/src/Pages/Cart.jsx
@@ -15,8 +15,20 @@ const Cart = () => {
     <div>
       <Navbar />
       <MobNav />
-      <Flex w="100%" justifyContent={"center"} gap="30px" mt="20px" fontSize={"14px"} >
-        <Grid w="50%" gap="10px" bgColor={"white"} p="10px">
+      <Flex
+        w="100%"
+        justifyContent={"center"}
+        flexDirection={{ base: "column", md: "row", lg: "row" }}
+        gap={{ base: "10px", md: "20px", lg: "30px" }}
+        mt={{ base: "10px", md: "20px", lg: "20px" }}
+        fontSize={"14px"}
+      >
+        <Grid
+          w={{ base: "100%", md: "65%", lg: "50%" }}
+          gap="10px"
+          bgColor={"white"}
+          p="10px"
+        >
           {products.length > 0 &&
             products?.map((elem) => {
               return (
@@ -28,7 +40,12 @@ const Cart = () => {
                   borderRadius="5px"
                   gap="10px"
                 >
-                  <Image src={elem.image} w={{ base: "", md: "", lg: "28%" }} />
+                  <Box w={{ base: "30%", md: "28%", lg: "28%" }}>
+                    <Image
+                      src={elem.image}
+                      w={{ base: "100%", md: "100%", lg: "100%" }}
+                    />
+                  </Box>
                   <Grid justifyContent={"space-between"}>
                     <Box>
                       <Text color="#666666">Product by {elem.brand}</Text>
@@ -38,7 +55,25 @@ const Cart = () => {
                       {elem.discount !== 0 && (
                         <Text color="green">-{elem.discount}%</Text>
                       )}
-                      <Text fontWeight={"500"}>₹{Math.floor(elem.price)}</Text>
+
+                      {elem.discount !== 0 && (
+                        <Text
+                          color="#B096AB"
+                          fontSize={"13px"}
+                          textDecoration={"line-through"}
+                        >
+                          {Math.floor(elem.price)}
+                        </Text>
+                      )}
+
+                      <Text fontWeight={"500"}>
+                        ₹
+                        {elem.discount !== 0
+                          ? Math.floor(
+                              elem.price - (elem.price / 100) * elem.discount
+                            )
+                          : elem.price}
+                      </Text>
                     </Flex>
 
                     <Box>
@@ -70,15 +105,19 @@ const Cart = () => {
                       </Text>
                     )}
                     <Flex gap="2px">
-                      <Button size="xs" >Move to Wishlist</Button>
-                      <Button size="xs" >Remove</Button>
+                      <Button size="xs">Move to Wishlist</Button>
+                      <Button size="xs">Remove</Button>
                     </Flex>
                   </Grid>
                 </Flex>
               );
             })}
         </Grid>
-        <Box border="1px solid black" w="25%"></Box>
+        <Box
+          // border="1px solid black"
+          w={{ base: "100%", md: "28%", lg: "25%" }}
+          bgColor="white"
+        ></Box>
       </Flex>
     </div>
   );
