7a58a3ceef82767f2885078f5c0726792316f2dc
Ayush Verma <ayush98277@gmail.com>
2023-10-01 17:52:54 +0530

    fixed the cookies issues

--- meetgod/src/Redux/AuthReducer/action.js
+++ meetgod/src/Redux/AuthReducer/action.js
@@ -8,30 +8,34 @@ export const userSignupPost = (userData) => (dispatch) => {
     .then((res) => {
       console.log(res);
       console.log("res from action", res);
-      dispatch({type: types.POST_SIGNUP_SUCCESS, payload: res.data.message})
+      dispatch({ type: types.POST_SIGNUP_SUCCESS, payload: res.data.message });
     })
     .catch((err) => {
       console.log(err);
-      dispatch({type: types.POST_SIGNUP_ERROR})
+      dispatch({ type: types.POST_SIGNUP_ERROR });
     });
 };
 
 export const userLoginPost = (userData) => (dispatch) => {
   dispatch({ type: types.POST_LOGIN_REQUEST });
   axios
-    .post("https://sleepy-clam-kimono.cyclic.app/user/login", userData)
+    .post("http://localhost:5050/user/login", userData, {
+      withCredentials: true,
+    })
     .then((res) => {
       console.log(res);
       console.log("res from action", res);
-      dispatch({type: types.POST_LOGIN_SUCCESS, payload: res.data})
+      dispatch({ type: types.POST_LOGIN_SUCCESS, payload: res.data });
     })
     .catch((err) => {
       console.log(err);
-      dispatch({type: types.POST_LOGIN_ERROR, payload: err.response.data.message})
+      dispatch({
+        type: types.POST_LOGIN_ERROR,
+        payload: err.response.data.message,
+      });
     });
 };
 
-export const signupReset = ()=>(dispatch)=>{
-  dispatch({type: types.POST_SIGNUP_RESET})
-}
-
+export const signupReset = () => (dispatch) => {
+  dispatch({ type: types.POST_SIGNUP_RESET });
+};
